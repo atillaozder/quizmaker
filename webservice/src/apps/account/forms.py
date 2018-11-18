b9bb3a273890e226fcce6eccd6479bd1b693f0bf
Atilla Özder <atillanevzat@gmail.com>
2018-11-18 15:35:48 +0300

    User Password Reset
    Password reset email and reset link was implemented.

--- webservice/src/apps/account/forms.py
+++ webservice/src/apps/account/forms.py
@@ -10,7 +10,7 @@ class AdminCreationForm(forms.ModelForm):
     A form for creating new users. Includes all the required
     fields, plus a repeated password.
     """
-    password1 = forms.CharField(label=_('Your Password'), widget=forms.PasswordInput)
+    password1 = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
     password2 = forms.CharField(label=_('Repeat Password'), widget=forms.PasswordInput)
 
     class Meta:
--- webservice/src/apps/question/apps.py
+++ webservice/src/apps/question/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class QuestionConfig(AppConfig):
     name = 'question'
+
+    def ready(self):
+        import apps.question.signals
