1197982ff9e80069f234742936fff504113a9690
Atilla Özder <atillanevzat@gmail.com>
2018-11-21 16:03:25 +0300

    Commit

---
+++ quizmaker/quizmaker/Models/EditProfile.swift
@@ -0,0 +1,9 @@
+//
+//  EditProfile.swift
+//  quizmaker
+//
+//  Created by Atilla Özder on 21.11.2018.
+//  Copyright © 2018 Atilla Özder. All rights reserved.
+//
+
+import Foundation
---
+++ quizmaker/quizmaker/Models/Participant.swift
@@ -0,0 +1,9 @@
+//
+//  Participant.swift
+//  quizmaker
+//
+//  Created by Atilla Özder on 21.11.2018.
+//  Copyright © 2018 Atilla Özder. All rights reserved.
+//
+
+import Foundation
