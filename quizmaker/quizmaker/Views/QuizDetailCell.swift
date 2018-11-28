b8336b590c5ceaf87cadb90f25ae289bc7397512
selinAtalay <33281865+selinAtalay@users.noreply.github.com>
2018-11-28 16:38:24 +0300

    Quiz Detail Cell

--- quizmaker/quizmaker/Views/QuizDetailCell.swift
+++ quizmaker/quizmaker/Views/QuizDetailCell.swift
@@ -1,2 +1,79 @@
+import UIKit
 
-import Foundation
+class QuizDetailTableCell: UITableViewCell {
+    
+    let nameLabel: UILabel = {
+        return UILabel.uiLabel(1, .byTruncatingTail, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    let descriptionLabel: UILabel = {
+        return UILabel.uiLabel(0, .byWordWrapping, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    let startDateLabel: UILabel = {
+        return UILabel.uiLabel(1, .byTruncatingTail, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    let endDateLabel: UILabel = {
+        return UILabel.uiLabel(1, .byTruncatingTail, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    let courseNameLabel: UILabel = {
+        return UILabel.uiLabel(1, .byTruncatingTail, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    let percentageLabel: UILabel = {
+        return UILabel.uiLabel(1, .byTruncatingTail, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    let ownerNameLabel: UILabel = {
+        return UILabel.uiLabel(1, .byTruncatingTail, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        self.accessoryType = .none
+        
+        let arrangedSubviews = [
+            nameLabel,
+            descriptionLabel,
+            ownerNameLabel,
+            courseNameLabel,
+            startDateLabel,
+            endDateLabel,
+            percentageLabel,
+            ]
+        
+        let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        contentView.addSubview(stackView)
+        stackView.fillSafeArea(spacing: .init(top: 10, left: 16, bottom: -10, right: -16), size: .zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func configure(_ element: Quiz) {
+        nameLabel.text = "Name: \(element.name)"
+        descriptionLabel.text = "Description:\n\(element.description ?? "")"
+        ownerNameLabel.text = "Quiz Created By: \(element.ownerName)"
+        
+        if let courseName = element.courseName {
+            courseNameLabel.text = "Course Name: \(courseName)"
+        }
+        
+        percentageLabel.text = "Percentage: \(element.percentage)"
+        
+        let startDate = DateFormatter.localizedString(from: element.start, dateStyle: .medium, timeStyle: .medium)
+        startDateLabel.text = "Starts: \(startDate)"
+        
+        let endDate = DateFormatter.localizedString(from: element.end, dateStyle: .medium, timeStyle: .medium)
+        endDateLabel.text = "Ends: \(endDate)"
+    }
+}
