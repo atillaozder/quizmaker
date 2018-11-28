452d4c1a81876c333e92550b13540235d5f569c8
MertCetinkya <43878831+MertCetinkya@users.noreply.github.com>
2018-11-28 16:59:21 +0300

    Checkbox

--- quizmaker/quizmaker/Views/Checkbox.swift
+++ quizmaker/quizmaker/Views/Checkbox.swift
@@ -1,2 +1,25 @@
+import UIKit
 
-import Foundation
+class CheckBox: UIButton {
+
+    let checkedImage: UIImage = UIImage(imageLiteralResourceName: "checkbox_checked")
+    let uncheckedImage: UIImage  = UIImage(imageLiteralResourceName: "checkbox_unchecked")
+    
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, for: .normal)
+            } else {
+                self.setImage(uncheckedImage, for: .normal)
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
