66856a1fe26584afa9c78ef8da4e837318af1ec9
selinAtalay <33281865+selinAtalay@users.noreply.github.com>
2018-11-28 16:37:26 +0300

    Course Table Cell

--- quizmaker/quizmaker/Views/CourseTableCell.swift
+++ quizmaker/quizmaker/Views/CourseTableCell.swift
@@ -1,2 +1,13 @@
+import UIKit
 
-import Foundation
+class CourseTableCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .gray
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
