16e1aa654f0dcb0735ac7e9052bb7f48a0c34b2c
selinAtalay <33281865+selinAtalay@users.noreply.github.com>
2018-11-28 17:23:07 +0300

    Course VC

--- quizmaker/quizmaker/ViewControllers/Course/CourseViewController.swift
+++ quizmaker/quizmaker/ViewControllers/Course/CourseViewController.swift
@@ -1,2 +1,77 @@
+import UIKit
+import RxCocoa
+import RxSwift
 
-import Foundation
+private let courseCell = "courseCell"
+
+class CourseViewController: UIViewController {
+    
+    private let viewModel = CourseViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private let tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .plain)
+        tv.backgroundColor = .white
+        tv.rowHeight = 50
+        tv.estimatedRowHeight = 50
+        tv.sectionHeaderHeight = 0
+        tv.sectionFooterHeight = 0
+        tv.separatorStyle = .singleLine
+        tv.separatorInset = .zero
+        tv.bounces = false
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "My Courses"
+        self.view.backgroundColor = .white
+        
+        tableView.register(CourseTableCell.self, forCellReuseIdentifier: courseCell)
+        self.view.addSubview(tableView)
+        tableView.fillSafeArea()
+        
+        self.navigationItem.backBarButtonItem = UIBarButtonItem()
+        
+        var frame = CGRect.zero
+        frame.size.height = .leastNormalMagnitude
+        tableView.tableHeaderView = UIView(frame: frame)
+        tableView.tableFooterView = UIView(frame: frame)
+        
+        viewModel.items
+            .asDriver()
+            .drive(tableView.rx.items(cellIdentifier: courseCell, cellType: CourseTableCell.self)) { (_, element, cell) in
+                cell.accessoryType = .disclosureIndicator
+                cell.textLabel?.text = element.name
+                cell.detailTextLabel?.text = "Number of students: \(element.students.count)"
+            }.disposed(by: disposeBag)
+        
+        viewModel.failure
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] (error) in
+                switch error {
+                case .apiMessage(let response):
+                    self.showErrorAlert(message: response.message)
+                case .api(let response):
+                    self.showErrorAlert(message: response.errorDesc)
+                default:
+                    self.showErrorAlert()
+                }
+            }).disposed(by: disposeBag)
+        
+        Observable.zip(tableView.rx.itemSelected, tableView.rx.modelSelected(Course.self))
+            .subscribe(onNext: { [weak self] (indexPath, item) in
+                guard let strongSelf = self else { return }
+                strongSelf.tableView.deselectRow(at: indexPath, animated: true)
+                
+                let viewController = MyQuizListViewController(courseID: item.id, courseName: item.name)
+                strongSelf.navigationController?.pushViewController(viewController, animated: true)
+                
+            }).disposed(by: disposeBag)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.loadPageTrigger.onNext(())
+    }
+}
--- quizmaker/quizmaker/ViewControllers/Quiz/QuizDetailViewController.swift
+++ quizmaker/quizmaker/ViewControllers/Quiz/QuizDetailViewController.swift
@@ -1,2 +1,181 @@
+import UIKit
+import RxCocoa
+import RxSwift
+import RxDataSources
 
-import Foundation
+private let quizDetailCell = "quizDetailCell"
+private let quizQuestionCell = "quizQuestionCell"
+private let quizParticipantCell = "quizParticipantCell"
+
+class QuizDetailViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    private let viewModel: QuizDetailViewModel
+    
+    private let tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .grouped)
+        tv.backgroundColor = .white
+        tv.rowHeight = UITableView.automaticDimension
+        tv.estimatedRowHeight = 100
+        tv.sectionHeaderHeight = 50
+        tv.sectionFooterHeight = 0
+        tv.separatorStyle = .none
+        tv.separatorInset = .zero
+        return tv
+    }()
+    
+    init(quiz: Quiz) {
+        viewModel = QuizDetailViewModel(quiz: quiz)
+        super.init(nibName: nil, bundle: nil)
+        self.navigationItem.title = "\(quiz.name)"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        let delete = UIImage(imageLiteralResourceName: "delete")
+        let update = UIImage(imageLiteralResourceName: "update")
+        
+        let deleteBarButton = UIBarButtonItem(image: delete, style: .plain, target: self, action: #selector(deleteTapped))
+        let updateBarButton = UIBarButtonItem(image: update, style: .plain, target: self, action: #selector(updateTapped))
+        
+        self.navigationItem.setRightBarButtonItems([deleteBarButton, updateBarButton], animated: false)
+        
+        tableView.register(QuizDetailTableCell.self, forCellReuseIdentifier: quizDetailCell)
+        tableView.register(QuizQuestionTableCell.self, forCellReuseIdentifier: quizQuestionCell)
+        tableView.register(QuizParticipantTableCell.self, forCellReuseIdentifier: quizParticipantCell)
+        
+        tableView.bounces = false
+        
+        self.view.addSubview(tableView)
+        tableView.fillSafeArea()
+        
+        bindUI()
+    }
+    
+    private func bindUI() {
+        viewModel.loadPageTrigger.onNext(())
+
+        let dataSource = RxTableViewSectionedReloadDataSource<DetailSectionModel>.init(configureCell: { (dataSource, tableView, indexPath, section) -> UITableViewCell in
+            
+            switch section {
+            case .detail(let quiz):
+                
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: quizDetailCell, for: indexPath) as? QuizDetailTableCell else { return UITableViewCell() }
+                
+                cell.configure(quiz)
+                return cell
+            case .participants(let participants):
+                
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: quizParticipantCell, for: indexPath) as? QuizParticipantTableCell else { return UITableViewCell() }
+                
+                cell.delegate = self
+                cell.configure(participants)
+                
+                return cell
+            case .questions(let questions):
+                
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: quizQuestionCell, for: indexPath) as? QuizQuestionTableCell else { return UITableViewCell() }
+                
+                cell.configure(questions)
+                
+                return cell
+            }
+        })
+        
+        viewModel.items
+            .asDriver()
+            .drive(tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        
+        
+        viewModel.failure
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] (error) in
+                switch error {
+                case .apiMessage(let response):
+                    self.showErrorAlert(message: response.message)
+                case .api(let response):
+                    self.showErrorAlert(message: response.errorDesc)
+                default:
+                    self.showErrorAlert()
+                }
+            }).disposed(by: disposeBag)
+        
+        viewModel.success
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] (_) in
+                
+                let alertController = UIAlertController(title: "Success", message: "Quiz deleted successfully.", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "OK", style: .destructive, handler: { (_) in
+                    self.navigationController?.popViewController(animated: true)
+                })
+                
+                alertController.addAction(ok)
+                self.present(alertController, animated: true, completion: nil)
+                
+            }).disposed(by: disposeBag)
+        
+        tableView.delegate = self
+    }
+    
+    @objc
+    private func deleteTapped() {
+        let alertController = UIAlertController(title: "Are you sure?", message: "You sure that you want to delete quiz \(viewModel.quiz.name)?", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let yes = UIAlertAction(title: "Yes, I am sure", style: .default) { (_) in
+            self.viewModel.deleteTrigger.onNext(())
+        }
+        
+        alertController.addAction(cancel)
+        alertController.addAction(yes)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc
+    private func updateTapped() {
+        let now = Date()
+        if viewModel.quiz.start < now && viewModel.quiz.end > now {
+            self.showErrorAlert(message: "The quiz has already started and not finished yet. You have to wait until its finishes.")
+        } else {
+            let viewController = QuizCreateViewController(quiz: viewModel.quiz)
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+}
+
+extension QuizDetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UITableViewHeaderFooterView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 50))
+
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let headerView = view as? UITableViewHeaderFooterView {
+            let title = UILabel()
+            title.text = viewModel.items.value[section].title
+            title.font = UIFont.preferredFont(forTextStyle: .headline)
+            
+            headerView.contentView.addSubview(title)
+            title.fillSuperView(spacing: .init(top: 0, left: 16, bottom: 0, right: -16), size: .zero)
+        }
+    }
+}
+
+extension QuizDetailViewController: QuizParticipantTableCellDelegate {
+    func didTapParticipant(_ participant: QuizParticipant) {
+        guard let user = participant.participant else { return }
+        let viewController = QuizParticipantAnswersViewController(id: user.id, name: user.username)
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+}
