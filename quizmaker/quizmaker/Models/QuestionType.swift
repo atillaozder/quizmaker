dd4d95c5625ab5fbc571df12e1bab9142cc34fd1
minabilgin <begum.bilgin@isik.edu.tr>
2018-11-28 17:09:00 +0200

    Question Type

--- quizmaker/quizmaker/Models/QuestionType.swift
+++ quizmaker/quizmaker/Models/QuestionType.swift
@@ -1,2 +1,18 @@
-
 import Foundation
+
+enum QuestionType: String {
+    case multichoice = "multichoice"
+    case truefalse = "truefalse"
+    case text = "text"
+    
+    var description: String {
+        switch self {
+        case .multichoice:
+            return "Multichoice"
+        case .text:
+            return "Text"
+        case .truefalse:
+            return "True False"
+        }
+    }
+}
