d64ae3e3ab8f5f68c3f2decdc0491c79a48e6c99
minabilgin <begum.bilgin@isik.edu.tr>
2018-11-28 17:10:26 +0200

    Quiz Question Detail Table Cell

--- quizmaker/quizmaker/Views/QuizQuestionDetailTableCell.swift
+++ quizmaker/quizmaker/Views/QuizQuestionDetailTableCell.swift
@@ -1,2 +1,40 @@
+import UIKit
 
-import Foundation
+class QuizQuestionDetailTableCell: UITableViewCell {
+    
+    let questionLabel: UILabel = {
+        return UILabel.uiLabel(2, .byTruncatingTail, "", .left, .black, .preferredFont(forTextStyle: .subheadline), true, false)
+    }()
+    
+    let questionTypeLabel: UILabel = {
+        return UILabel.uiLabel(1, .byTruncatingTail, "", .left, .gray, .preferredFont(forTextStyle: .footnote), true, false)
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        
+        let arrangedSubviews = [
+            questionLabel,
+            questionTypeLabel
+            ]
+        
+        let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        contentView.addSubview(stackView)
+        stackView.fillSafeArea(spacing: .init(top: 10, left: 16, bottom: -10, right: -16), size: .zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func configure(_ element: Question) {
+        questionLabel.text = element.question
+        questionTypeLabel.text = element.questionType
+    }
+}
