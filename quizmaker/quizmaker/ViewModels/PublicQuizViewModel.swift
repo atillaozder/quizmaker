16fffa3332920a679a60ba242791b45f6f009a0b
selinAtalay <33281865+selinAtalay@users.noreply.github.com>
2018-12-05 16:05:55 +0300

    Public Quiz View Model

--- quizmaker/quizmaker/ViewModels/PublicQuizViewModel.swift
+++ quizmaker/quizmaker/ViewModels/PublicQuizViewModel.swift
@@ -1,2 +1,60 @@
+import RxSwift
+import RxCocoa
 
-import Foundation
+class PublicQuizViewModel {
+    
+    private let disposeBag = DisposeBag()
+    
+    let items: BehaviorRelay<[QuizSectionModel]>
+    let success: PublishSubject<Void>
+    let failure: PublishSubject<NetworkError>
+    let loadPageTrigger: PublishSubject<Void>
+    
+    init() {
+        items = BehaviorRelay(value: [])
+        failure = PublishSubject()
+        success = PublishSubject()
+        loadPageTrigger = PublishSubject()
+        
+        loadPageTrigger.asObservable()
+            .flatMap { [unowned self] (_) -> Observable<[QuizSectionModel]> in
+                let endpoint: QuizEndpoint = .all
+                return self.fetch(endpoint)
+            }.bind(to: items)
+            .disposed(by: disposeBag)
+    }
+    
+    private func fetch(_ endpoint: QuizEndpoint) -> Observable<[QuizSectionModel]> {
+        return Observable.create({ [weak self] (observer) -> Disposable in
+            guard let strongSelf = self else { return Disposables.create() }
+            NetworkManager.shared.request(endpoint, [Quiz].self)
+                .subscribe(onNext: { (result) in
+                    switch result {
+                    case .success(let object):
+                        var sectionModel: [QuizSectionModel] = []
+                        object.forEach({ (quiz) in
+                            sectionModel.append(.quiz(item: quiz))
+                        })
+                        
+                        observer.onNext(sectionModel)
+                    case .failure(let error):
+                        strongSelf.failure.onNext(error)
+                    }
+                }).disposed(by: strongSelf.disposeBag)
+            return Disposables.create()
+        })
+    }
+    
+    func append(_ quizId: Int) {
+        let endpoint = QuizEndpoint.append(quizID: quizId)
+        NetworkManager.shared.requestJSON(endpoint, .apiMessage)
+            .subscribe(onNext: { [weak self] (result) in
+                switch result {
+                case .success:
+                    self?.success.onNext(())
+                case .failure(let error):
+                    self?.failure.onNext(error)
+                }
+            }).disposed(by: disposeBag)
+    }
+}
