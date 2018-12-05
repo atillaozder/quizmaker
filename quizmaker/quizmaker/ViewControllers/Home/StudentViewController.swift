2fa28e63dc0d888025a5d8782dd5b4d3250d98db
minabilgin <begum.bilgin@isik.edu.tr>
2018-12-05 15:25:26 +0300

    Update StudentViewController

--- quizmaker/quizmaker/ViewControllers/Home/StudentViewController.swift
+++ quizmaker/quizmaker/ViewControllers/Home/StudentViewController.swift
@@ -17,10 +17,17 @@ class StudentViewController: UserViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        lectureQuizzesButton.addTarget(self, action: #selector(lectureQuizzesTapped), for: .touchUpInside)
     }
     
     override func setupViews() {
         super.setupViews()
         lastStackView.addArrangedSubview(lectureQuizzesButton)
+    }
+    
+    @objc
+    private func lectureQuizzesTapped() {
+        let viewController = MyLecturesViewController()
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 }
